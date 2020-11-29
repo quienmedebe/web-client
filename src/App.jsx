c00574e1a47545d2b8ddb10d09b0c714d7492378
Raúl Contreras Rubio <raulcontrerasrubio@gmail.com>
2020-11-29 19:41:31 +0100

    Feature/navigation routes (#14)
    * Add app state context
    
    * Add redux
    
    * Remove unused variables
    
    * Create a basic router with two routes and state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import {useSelector} from 'react-redux';
+import GlobalStyles from './GlobalStyles';
+import Navigation from './Navigation';
 
 function App() {
+  const appState = useSelector(state => state.app);
+
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src={logo} className='App-logo' alt='logo' />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyles />
+      <Navigation loggedUserId={appState.loggedId} />
+    </>
   );
 }
 
