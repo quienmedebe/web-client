397d06d99f249bb00598a995fd20d3767aeca6c3
Raúl Contreras Rubio <raulcontrerasrubio@gmail.com>
2020-12-07 11:39:11 +0100

    Create remember password styles (#21)

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -5,6 +5,7 @@ import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import store from './redux/store';
 import {getCurrentAppState} from './redux/app/app.functions';
+import RememberPassword from './pages/RememberPassword/RememberPassword';
 
 const authGuard = (to, from, next) => {
   if (!to.meta?.auth) {
@@ -31,6 +32,7 @@ const Navigation = () => {
         <GuardedRoute exact path='/' component={Home} meta={{auth: true}} />
         <GuardedRoute exact path='/guarded' component={Home} meta={{auth: true}} />
         <GuardedRoute exact path='/login' component={Login} />
+        <GuardedRoute exact path='/remember-password' component={RememberPassword} />
         <GuardedRoute render={() => <Redirect to='/' />} />
       </Switch>
     </GuardProvider>
